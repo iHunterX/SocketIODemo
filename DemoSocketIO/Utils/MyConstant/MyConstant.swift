69ac119e678aace87ae44051a7a0c15342857e22
Dinh Xuan Loc <dxloc@keizu.co.jp>
2016-10-24 15:52:38 +0700

    update folder

---
+++ DemoSocketIO/Utils/MyConstant/MyConstant.swift
@@ -0,0 +1,13 @@
+//
+//  MyConstant.swift
+//  DemoSocketIO
+//
+//  Created by Loc.dx-KeizuDev on 10/24/16.
+//  Copyright © 2016 Loc Dinh Xuan. All rights reserved.
+//
+
+import UIKit
+
+class MyConstant: NSObject {
+
+}
